2e5cd240cd3ab30bed8f815d26a6b0a5aa39a520
Wilfred007 <adzerwilfred007@gmail.com>
2025-12-24 16:14:08 +0100

    sonner

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 import Landing from './pages/Landing';
+import { Toaster } from 'sonner';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
+    <Toaster richColors position="top-right" expand={false} duration={3500} />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
